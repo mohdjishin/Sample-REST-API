62783282f4745bb1ca5c857b6810a808229f9b61
mohdjishin <whoistcp@protonmail.com>
2022-05-12 18:40:55 +0530

    2 value search api

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
-from fastapi import FastAPI
+from unicodedata import name
+from fastapi import FastAPI, Query
 from pydantic import BaseModel
 from typing import Optional
 
@@ -18,8 +19,26 @@ with open('people.json', 'r') as f:
     people = json.load(f)['people']
 
 
-@app.get('/person/{p_id}')
+@app.get('/person/{p_id}', status_code=200)
 def get_person(p_id: int):
     person = [p for p in people if p['id'] == p_id]
     return person[0] if len(person)> 0 else{}
+
+@app.get("/search",status_code=200)
+def search_person(age: Optional[int]= Query(None,title="Age",description="The age to filter for"),
+                  name: Optional[str] = Query(None,title="name",description="The name")):
+                  People1 = [p for p in people if p['age'] == age ]
+
+                  if name is None:
+                      if age is None:
+                          return people
+                      else:
+                          return People1
+                  else:
+                      People2= [p for p in people if name.lower() in p['name'].lower()]
+                      if age is None:
+                          return People2
+                      else:
+                          combined = [p for p in People1 if p in People2]
+                          return combined
 
