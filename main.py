7fb1e03cf83f1d37c641a59aaca1f8e9c1e95c5d
mohdjishin <whoistcp@protonmail.com>
2022-05-12 17:58:50 +0530

    first stage api

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
+import json
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
-
-@app.get('/')
-def hello():
-    return {"Hello":"World"}
-
-@app.post('/')
-def helo_post():
-    return {"success": "POSTED!"}
+class Person(BaseModel):
+    id: Optional[int] =None
+    name:str
+    age:int
+    gender:str
 
 
-@app.get('/something')
-def something():
-    return {"Data":"Something"}
+
+
+with open('people.json', 'r') as f:
+    people = json.load(f)['people']
+
+
+@app.get('/person/{p_id}')
+def get_person(p_id: int):
+    person = [p for p in people if p['id'] == p_id]
+    return person[0] if len(person)> 0 else{}
+
